44a6d1339242b1932dae39eaab4168c26fa03d77
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-21 18:27:36 -0300

    feat: create and add FilterBar component to main

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,9 +1,10 @@
+import FilterBar from '@/components/FilterBar';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Olá mundo.</h1>
+      <FilterBar />
     </main>
   );
 }
---
+++ capputeeno/src/components/FilterBar.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { styled } from 'styled-components';
+import { memo } from 'react';
+import FilterByType from './FilterByType';
+import FilterByPriority from './FilterByPriority';
+
+const FilterContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: start;
+  justify-content: space-between;
+`;
+
+interface FilterBarProps {}
+function FilterBar(props: FilterBarProps) {
+  return (
+    <FilterContainer>
+      <FilterByType />
+      <FilterByPriority />
+    </FilterContainer>
+  );
+}
+
+export default memo(FilterBar);
