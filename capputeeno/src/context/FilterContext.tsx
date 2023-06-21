55f1ef54182f8d8834b3beae83441ab0904dafe4
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-06-21 18:20:58 -0300

    feat: create FilterContext

---
+++ capputeeno/src/context/FilterContext.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { FilterType, PriorityType } from '@/types/FilterTypes';
+import { createContext, useState } from 'react';
+
+export const FilterContext = createContext({
+  search: '',
+  page: 0,
+  type: FilterType.ALL,
+  priority: PriorityType.NEWS,
+  setSearch: (value: string) => {},
+  setPage: (value: number) => {},
+  setType: (value: FilterType) => {},
+  setPriority: (value: PriorityType) => {},
+});
+
+interface FilterTypeContextProviderProps {
+  children: React.ReactNode;
+}
+
+export function FilterTypeContextProvider({
+  children,
+}: FilterTypeContextProviderProps) {
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(0);
+  const [type, setType] = useState(FilterType.ALL);
+  const [priority, setPriority] = useState(PriorityType.NEWS);
+
+  return (
+    <FilterContext.Provider
+      value={{
+        search,
+        page,
+        type,
+        priority,
+        setSearch,
+        setPage,
+        setType,
+        setPriority,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+}
