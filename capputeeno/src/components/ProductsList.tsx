12faa87b77f94f1b55eec6adc2e0cab0ac54d9e1
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-21 18:49:28 -0300

    feat: add ProductCard to ProductList

--- capputeeno/src/components/ProductsList.tsx
+++ capputeeno/src/components/ProductsList.tsx
@@ -1,13 +1,35 @@
 'use client';
 
 import { useProducts } from '@/hooks/useProducts';
+import ProdutctCard from './ProdutctCard';
+import { styled } from 'styled-components';
 
 interface ProductsListProps {}
+
+const ProductsSection = styled.section`
+  display: flex;
+  justify-content: center;
+  gap: 32px;
+  width: 100%;
+  flex-wrap: wrap;
+  margin-top: 32px;
+`;
 
 export function ProductsList(props: ProductsListProps) {
   const { data } = useProducts();
 
   console.log(data);
 
-  return <></>;
+  return (
+    <ProductsSection>
+      {data?.map((product) => (
+        <ProdutctCard
+          key={product?.id}
+          image={product?.imageUrl}
+          price={product?.priceInCents}
+          title={product?.name}
+        />
+      ))}
+    </ProductsSection>
+  );
 }
