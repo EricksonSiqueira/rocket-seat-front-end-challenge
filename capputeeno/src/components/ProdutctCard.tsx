f64510b32bb0fb2e8000dac9df5320bbb72fb53b
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-21 18:48:51 -0300

    feat: create component ProductCard

---
+++ capputeeno/src/components/ProdutctCard.tsx
@@ -0,0 +1,88 @@
+import { styled } from 'styled-components';
+import { memo } from 'react';
+import Image from 'next/image';
+
+interface ProductCardProps {
+  image: string;
+  title: string;
+  price: number;
+}
+
+const Card = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 8px 8px 0px 0px;
+  background: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(10px);
+`;
+
+const CardProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  row-gap: 8px;
+  padding: 8px 12px;
+  background-color: white;
+  border-radius: 0px 0px 4px 4px;
+
+  > div {
+    width: 100%;
+    height: 1px;
+    background-color: var(--shapes-02);
+  }
+`;
+
+const CardImageWrapper = styled.div`
+  width: 256px;
+  height: 300px;
+  position: relative;
+  border-radius: 8px 8px 0px 0px;
+
+  .image {
+    border-radius: 8px 8px 0px 0px;
+    object-fit: cover;
+  }
+`;
+
+const CardTitle = styled.p`
+  color: var(dark-texts-inputs-icons);
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 24px;
+`;
+
+const CardPrice = styled.p`
+  color: var(--shapes-dark-tooltip);
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 21px;
+`;
+
+const formatarValor = (valor: number) =>
+  `R$ ${Math.floor(valor / 100)},${(valor % 100).toString().padStart(2, '0')}`;
+
+function ProductCard(props: ProductCardProps) {
+  return (
+    <Card>
+      <CardImageWrapper>
+        <Image
+          src={props.image}
+          alt={'imagem do produto'}
+          fill
+          className="image"
+        />
+      </CardImageWrapper>
+      <CardProductInfo>
+        <CardTitle>{props.title}</CardTitle>
+        <div />
+        <CardPrice>{formatarValor(props.price)}</CardPrice>
+      </CardProductInfo>
+    </Card>
+  );
+}
+
+export default memo(ProductCard);
