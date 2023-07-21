8eb27ba45a872298fa489e6c51c4519d7235deee
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-21 14:43:40 -0300

    feat: create hook useProducts

---
+++ capputeeno/src/hooks/useProducts.tsx
@@ -0,0 +1,29 @@
+import { ProductsFetchResponse } from '@/types/ProductsResponse';
+import { useQuery } from '@tanstack/react-query';
+import axios, { AxiosPromise } from 'axios';
+import { camelizeKeys } from 'humps';
+
+const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
+  return axios.post(process.env.NEXT_PUBLIC_API_URL as string, {
+    query: `
+      query {
+        allProducts {
+          id
+          name
+          price_in_cents
+          image_url
+        }
+      }`,
+  });
+};
+
+export function useProducts() {
+  const { data } = useQuery({
+    queryFn: fetcher,
+    queryKey: ['products'],
+  });
+
+  const camelizedData = camelizeKeys(data?.data?.data?.allProducts);
+
+  return { data: camelizedData };
+}
