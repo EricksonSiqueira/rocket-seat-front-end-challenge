8742100e576e8b1a8b5f14ba0853655288844757
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-22 16:09:49 -0300

    feat: create component ImageWithLoading

---
+++ capputeeno/src/components/ImageWithLoading.tsx
@@ -0,0 +1,36 @@
+import { styled } from 'styled-components';
+import { memo, useState } from 'react';
+import Image from 'next/image';
+import Skeleton from './Skeleton';
+
+interface ImageWithLoadingProps {
+  src: string;
+  alt?: string;
+  priority?: boolean;
+  fill?: boolean;
+  className?: string;
+}
+
+const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+function ImageWithLoading(props: ImageWithLoadingProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      {isLoading ? <Skeleton className="imageSkeleton" /> : null}
+      <Image
+        src={props.src}
+        alt={props?.alt ?? 'imagem do produto'}
+        fill={props?.fill ?? false}
+        className={props?.className ?? 'image'}
+        priority={props?.priority ?? false}
+      />
+    </>
+  );
+}
+
+export default memo(ImageWithLoading);
