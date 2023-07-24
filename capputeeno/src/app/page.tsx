c7be60a156fbb0516e7e4f2f2d95d1b9038f544d
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-23 22:41:23 -0300

    refactor: use component DefaultPageLayout

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
 import FilterBar from '@/components/FilterBar';
-import styles from './page.module.css';
 import { ProductsList } from '@/components/ProductsList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DefaultPageLayout } from '@/components/DefaultPageLayout';
 
 export default function Home() {
   const client = new QueryClient();
 
   return (
     <QueryClientProvider client={client}>
-      <main className={styles.main}>
+      <DefaultPageLayout>
         <FilterBar />
         <ProductsList />
-      </main>
+      </DefaultPageLayout>
     </QueryClientProvider>
   );
 }
