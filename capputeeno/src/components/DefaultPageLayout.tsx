bdf336e52a63c17eb93010e655b8e06e868e3228
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-07-23 22:41:02 -0300

    feat: create component DefaultPageLayout to avoid repeating code

---
+++ capputeeno/src/components/DefaultPageLayout.tsx
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+export const DefaultPageLayout = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding: 0 16px;
+  margin: 0 auto;
+  min-height: 50vh;
+  max-width: 1184px;
+  row-gap: 22px;
+
+  @media (min-width: 768px) {
+    padding: 0 32px;
+  }
+`;
