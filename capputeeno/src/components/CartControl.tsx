b57d74eec0970a7254fbe233a64c8bfc48ceb4e2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-08-14 17:03:01 -0300

    feat: add router push to cart on cart icon

--- capputeeno/src/components/CartControl.tsx
+++ capputeeno/src/components/CartControl.tsx
@@ -3,6 +3,7 @@
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { ShoppingBag } from './Icons/ShoppingBag';
 import { styled } from 'styled-components';
+import { useRouter } from 'next/navigation';
 
 const CartCount = styled.span`
   position: absolute;
@@ -23,19 +24,27 @@ const CartCount = styled.span`
   line-height: 26px;
 `;
 
-const CartContainer = styled.div`
+const CartContainer = styled.button`
+  outline: none;
+  border: none;
+  background-color: transparent;
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-right: 8.5px;
+  cursor: pointer;
 `;
 
 export function CartControl() {
   const { value } = useLocalStorage('cart-items', ['2', '2']);
+  const router = useRouter();
 
   return (
-    <CartContainer>
+    <CartContainer
+      onClick={() => router.push('/carrinho')}
+      aria-label="Ir para página de carrinho."
+    >
       <ShoppingBag />
       {value.length > 0 ? <CartCount>{value.length}</CartCount> : null}
     </CartContainer>
