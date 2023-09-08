7251abf3608060d0d2838f0973f2a982f8a046f3
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-09-08 19:18:35 -0300

    style: add margim bottom to defualt page to create better ux

--- capputeeno/src/components/DefaultPageLayout.tsx
+++ capputeeno/src/components/DefaultPageLayout.tsx
@@ -9,6 +9,7 @@ export const DefaultPageLayout = styled.main`
   min-height: 50vh;
   max-width: 1184px;
   row-gap: 22px;
+  margin-bottom: 34px;
 
   @media (min-width: 768px) {
     padding: 0 32px;
